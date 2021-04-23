ca27c4b7ea0ae32580506994d25424469dd19833
Ayokunle <Ayoonadiji@gmail.com>
2021-04-23 18:28:05 +0100

    initial

---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session3.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+        public bool Confirmed { get; set; } = false;
+
+
+        //private static int nextId = 1;
+        //public Employee()
+        //{
+        //    Id = nextId;
+        //    nextId++;
+        //}
+    }
+}
